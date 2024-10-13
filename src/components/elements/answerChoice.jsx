e3edf8b90b84ba9b4f09baa0769e69882bb55148
Hasan Al Banna <lalbannal13@gmail.com>
2024-10-13 16:13:43 +0700

    last commit

--- src/components/elements/answerChoice.jsx
+++ src/components/elements/answerChoice.jsx
@@ -1,17 +1,17 @@
 export default function AnswerChoice({ answerTrue, answerFalse }) {
   return (
-    <div className="flex w-full h-[370px] justify-between">
+    <div className="flex w-full 2xl:h-[370px] h-[280px] justify-between">
       <button
-        className="w-[49%] flex justify-center items-center bg-green-600 rounded-lg text-6xl font-bold"
+        className="w-[49%] flex justify-center items-center bg-green-600 rounded-lg 2xl:text-6xl text-4xl font-bold"
         onClick={answerTrue}
       >
-        True
+        Benar
       </button>
       <button
-        className="w-[49%] flex justify-center items-center bg-red-600 rounded-lg text-6xl font-bold"
+        className="w-[49%] flex justify-center items-center bg-red-600 rounded-lg 2xl:text-6xl text-4xl font-bold"
         onClick={answerFalse}
       >
-        False
+        Salah
       </button>
     </div>
   );
--- src/components/elements/kuisCard.jsx
+++ src/components/elements/kuisCard.jsx
@@ -1,15 +1,17 @@
 export default function KuisCard({ currentQuiz, countdown, terjawab, soal }) {
   return (
-    <div className="w-full h-[400px] flex relative justify-center items-center bg-purple-700 bg-opacity-45 border border-white rounded-lg backdrop-brightness-65 p-8">
-      <div className="absolute right-5 top-5 text-xl font-bold bg-purple-700 bg-opacity-45 border border-white rounded-lg backdrop-brightness-65 p-5">
+    <div className="w-full 2xl:h-[400px] h-[250px] flex relative justify-center items-center bg-purple-700 bg-opacity-45 border border-white rounded-lg backdrop-brightness-65 p-8">
+      <div className="absolute right-5 top-5 2xl:text-xl text-md font-bold bg-purple-700 bg-opacity-45 border border-white rounded-lg backdrop-brightness-65 p-5">
         <span>
           Soal ke {terjawab + 1} / {soal}
         </span>
       </div>
-      <div className="absolute text-4xl font-semibold w-[80px] translate-x-0 top-[-40px] bg-purple-700 border border-white rounded-lg backdrop-brightness-65 p-4">
+      <div className="absolute 2xl:text-4xl text-2xl font-semibold w-[80px] translate-x-0 2xl:top-[-40px] top-[-30px] bg-purple-700 border border-white rounded-lg backdrop-brightness-65 p-4">
         <span className="flex justify-center">{countdown}</span>
       </div>
-      <h1 className="text-3xl font-bold text-center">{currentQuiz}</h1>
+      <h1 className="2xl:text-3xl text-xl font-bold text-center">
+        {currentQuiz}
+      </h1>
     </div>
   );
 }
